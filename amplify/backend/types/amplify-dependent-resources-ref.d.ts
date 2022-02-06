3d6621d88ce29a8b1fe2e7b54ef18a833b8949e2
ammar <ammarraneez@gmail.com>
2022-02-06 08:50:28 +0530

    fix: schema relationship

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "blogapi": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
